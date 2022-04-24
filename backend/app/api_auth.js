a16d428b24fcc5cb8fce75fa3fbea6b55bda0aa4
boonruang <scubatoy@gmail.com>
2022-04-25 01:12:28 +0700

    JWT middleware add

--- backend/app/api_auth.js
+++ backend/app/api_auth.js
@@ -8,7 +8,7 @@ const JwtConfig = require('../config/Jwt-Config')
 
 //  @route                  POST  /api/v2/auth/verify
 //  @desc                   User authorization verify & get token
-//  @access                 Public
+//  @access                 Private
 router.post('/verify', async (req, res) => {
   let userToken = req.headers['authorization']
   JWT.verify(userToken, JwtConfig.secret, (error, data) => {
